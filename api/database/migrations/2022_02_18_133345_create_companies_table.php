3a0a89f38b0b91a167aeea39c491a46af1e41edc
shogo-okugami <okugami365@gmail.com>
2022-02-18 22:46:27 +0900

    update:comaniesテーブルを作成するマイグレーションファイル
    companiesテーブルを作成するマイグレーションファイル

---
+++ api/database/migrations/2022_02_18_133345_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('email',255);
+            $table->softDeletes();
+            $table->unique(['email','deleted_at'],'companies_email_unique');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
