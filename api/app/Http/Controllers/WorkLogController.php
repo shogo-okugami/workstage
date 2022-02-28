4600f48268af57e43189134745762b351bb13d94
shogo-okugami <okugami365@gmail.com>
2022-02-28 12:49:30 +0900

    add:WorkLogController作成
    WorkLogController作成

---
+++ api/app/Http/Controllers/WorkLogController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkLog;
+use Illuminate\Http\Request;
+
+class WorkLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WorkLog  $workLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WorkLog $workLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\WorkLog  $workLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WorkLog $workLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WorkLog  $workLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WorkLog $workLog)
+    {
+        //
+    }
+}
