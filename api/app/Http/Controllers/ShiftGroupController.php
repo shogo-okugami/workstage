d488ba36a60d221fc887d3d29714e33973b25ace
shogo-okugami <okugami365@gmail.com>
2022-03-04 08:10:40 +0900

    add:ShiftGroupController作成
    ShiftGroupController作成

---
+++ api/app/Http/Controllers/ShiftGroupController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShiftGroup;
+use Illuminate\Http\Request;
+
+class ShiftGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ShiftGroup  $shiftGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ShiftGroup $shiftGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ShiftGroup  $shiftGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ShiftGroup $shiftGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ShiftGroup  $shiftGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ShiftGroup $shiftGroup)
+    {
+        //
+    }
+}
