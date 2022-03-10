a732be322add3fbaf01618e8aa82b346fcdc37bf
shogo-okugami <okugami365@gmail.com>
2022-03-10 23:37:10 +0900

    add:ShiftController作成
    ShiftController作成

---
+++ api/app/Http/Controllers/ShiftController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shift;
+use Illuminate\Http\Request;
+
+class ShiftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Shift  $shift
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Shift $shift)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Shift  $shift
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Shift $shift)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Shift  $shift
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Shift $shift)
+    {
+        //
+    }
+}
--- ui/components/layouts/Header.jsx
+++ ui/components/layouts/Header.jsx
@@ -8,6 +8,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
+import Drawer from '@mui/material/Drawer'
 
 const Header = () => {
 
