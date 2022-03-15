14cc32f56068528ad63a67b8020ee8ef31c293ea
shogo-okugami <okugami365@gmail.com>
2022-03-15 22:32:00 +0900

    update:orderカラムを追加
    orderカラムを追加

--- api/database/migrations/2022_03_05_103614_create_shifts_table.php
+++ api/database/migrations/2022_03_05_103614_create_shifts_table.php
@@ -17,6 +17,7 @@ class CreateShiftsTable extends Migration
             $table->id();
             $table->time('start_at');
             $table->time('end_at');
+            $table->integer('order');
             $table->foreinId('shift_group_id')->constrained();
             $table->foreinId('user_id')->constrained();
             $table->timestamps();
