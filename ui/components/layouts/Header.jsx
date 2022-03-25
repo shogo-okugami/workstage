444a0ee41206e0c1e2e12f9068fba34a9197af60
shogo-okugami <okugami365@gmail.com>
2022-03-25 23:06:11 +0900

    update:ドロワーメニューを追加
    ドロワーメニューを追加

--- ui/components/layouts/Header.jsx
+++ ui/components/layouts/Header.jsx
@@ -1,14 +1,85 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
+import MuiAppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import Drawer from '@mui/material/Drawer'
+import MuiDrawer from '@mui/material/Drawer'
+import Divider from '@mui/material/Divider';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import MailIcon from '@mui/icons-material/Mail';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { styled, useTheme } from '@mui/material/styles';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+})(({ theme }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
 
 const Header = () => {
 
@@ -22,18 +93,35 @@ const Header = () => {
     setAnchorEl(null);
   };
 
+  const theme = useTheme();
+
   const [open, setOpen] = React.useState(null);
 
+  const handleDrawer = () => {
+    setOpen(!open);
+  };
+
+  const items = [
+    { text: 'シフト', icon: <CalendarMonthIcon />, },
+    { text: '勤怠実績', icon: <WorkHistoryIcon />, },
+    { text: '打刻', icon: <AccessTimeIcon />, },
+    { text: 'メッセージ', icon: <MailIcon />, },
+    { text: 'メンバー', icon: <SupervisorAccountIcon />, },
+    { text: '設定', icon: <SettingsIcon />, },
+  ];
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ boxShadow: 0, bgcolor: 'primary.main', }} >
+    <Box>
+      <AppBar position="fixed" sx={{ boxShadow: 0, bgcolor: 'primary.main', }} >
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
-            aria-label="menu"
-            sx={{ mr: 5 }}
+            aria-label="open drawer"
+            onClick={handleDrawer}
+            edge="start"
+            sx={{
+              marginRight: 5,
+            }}
           >
             <MenuIcon />
           </IconButton>
@@ -72,6 +160,33 @@ const Header = () => {
           </div>
         </Toolbar>
       </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader />
+        <Divider />
+        <List>
+          {items.map((item) => (
+          <ListItemButton
+            key={item.text}
+            sx={{
+              minHeight: 48,
+              justifyContent: open ? 'initial' : 'center',
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: open ? 3 : 'auto',
+                justifyContent: 'center',
+              }}
+            >
+              {item.icon}
+            </ListItemIcon>
+            <ListItemText primary={item.text} sx={{ opacity: open ? 1 : 0 }} />
+          </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
     </Box >
   );
 }
