6b7286d3bd05b432b0b746329438180ee627ac06
shogo-okugami <okugami365@gmail.com>
2022-04-15 23:38:08 +0900

    update:ページ遷移時にローディングを表示
    ページ遷移時にローディングを表示

--- ui/components/layouts/Layout.jsx
+++ ui/components/layouts/Layout.jsx
@@ -15,6 +15,24 @@ const MainHeader = styled('div')(({ theme }) => ({
 
 const Layout = ({ children, title }) => {
 
+  const router = useRouter()
+  const [pageLoading, setPageLoading] = useState(false)
+
+  useEffect(() => {
+    const handleStart = (url) => url !== router.asPath && setPageLoading(true)
+    const handleComplete = () => setPageLoading(false)
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleComplete)
+      router.events.off('routeChangeError', handleComplete)
+    }
+  })
+
   return (
     <>
       <Head>
