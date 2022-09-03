130ae92266d7a29e607e64f62cc3e5426938a3e7
shogo-okugami <okugami365@gmail.com>
2022-09-03 23:03:38 +0900

    add:Dayコンポーネント作成
    Dayコンポーネントを作成

---
+++ ui/components/pages/Home/Calendar/Day.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useRouter } from "next/router";
+import AddCircleSharpIcon from '@mui/icons-material/AddCircleSharp';
+import { styled } from "@mui/material/styles";
+
+const CustomAddCircleIcon = styled(AddCircleSharpIcon)`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+`
+
+const Day = (props) => {
+
+  const { date,shiftDate } = props;
+  const router = useRouter();
+
+  const handleClick = () =>{
+    router.push(`/shift/${shiftDate}`);
+  }
+
+  return (
+    <>
+      <p>{date}</p>
+      <CustomAddCircleIcon color="primary" fontSize="large" onClick={handleClick} />
+    </>
+  );
+};
+
+export default Day;
